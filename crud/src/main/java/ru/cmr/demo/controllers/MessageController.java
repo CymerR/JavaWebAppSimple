7d31f868da576cc024eacfba02af3c57b8597832
cymerr <cymerr@CymerR-laptop>
2020-05-06 20:15:54 +0300

    added rest simple rest controller to Message entity. Message has an ID and text.

--- crud/src/main/java/ru/cmr/demo/controllers/MessageController.java
+++ crud/src/main/java/ru/cmr/demo/controllers/MessageController.java
@@ -1,8 +1,29 @@
 package ru.cmr.demo.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import ru.cmr.demo.entity.Message;
+
 @RestController	
+@RequestMapping("/messages")
 public class MessageController {
 	//TODO: Domain objects and http method to get them out
+	List<Message> messagesList = new ArrayList<Message>(){{
+		add(new Message("Hello"));
+		add(new Message("World"));
+		add(new Message("One more"));
+		add(new Message("Spring"));
+		add(new Message("Java"));
+	}};
+	
+	@GetMapping
+	public List<Message> getList(){
+		return messagesList;
+	}
+	
 }
---
+++ crud/src/main/java/ru/cmr/demo/entity/Message.java
@@ -0,0 +1,30 @@
+package ru.cmr.demo.entity;
+
+import javax.annotation.Generated;
+
+public class Message {
+	
+	static private long id_counter = 0;
+	private Long Id;
+	private String text;
+	public Message(String text) {
+		super();
+		this.text = text;
+		this.Id = id_counter++;
+	}
+	public Long getId() {
+		return Id;
+	}
+	public void setId(Long id) {
+		Id = id;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	
+	
+
+}
