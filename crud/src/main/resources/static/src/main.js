2f7ddfec806ba6695c68179e27666e59613c615f
cymerr <cymerr@CymerR-laptop>
2020-05-06 14:02:33 +0300

    some work on laptop. added vue-resource lib and vuetify. but it need to be reworked suddenly

--- crud/src/main/resources/static/src/main.js
+++ crud/src/main/resources/static/src/main.js
@@ -1,7 +1,12 @@
 import './styles/main.scss'
 
 import Vue from 'vue'
+import VueResource from 'vue-resource'
 import Main from './components/Main.vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+
+Vue.use(VueResource);
 
 new Vue({
   el: '#app',
